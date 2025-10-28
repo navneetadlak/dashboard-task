15d466bb52c274680e489b43b83774b6726f3cb1
Navneet Adlak <aniketadlak9@gmail.com>
2025-10-28 15:35:49 +0530

    settingup project folder structure

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>  
+      <div className="text-3xl text-red-500 font-bold underline">
+      testing tailwindcss
+      </div>
+    </>
+  )
+}
+
+export default App
